a05ba1d0254e4097a8f67e5e820d73a9793529f0
Arvin Zhou <ar.zhou@outlook.com>
2025-08-08 17:10:29 +0800

    fix: add default yes option

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "null_resource" "install_docker_k3s" {
       "  sudo apt-get update",
       "  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
       "  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "  sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'",
+      "  sudo add-apt-repository -y 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'",
       "  sudo apt-get update",
       "  sudo apt-get install -y docker-ce",
       "  sudo systemctl start docker",
